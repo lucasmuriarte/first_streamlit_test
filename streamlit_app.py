1cc455f54d98c21a68b6b350997608e246e96fa7
Lucas Martinez Uriarte <56222270+lucasmuriarte@users.noreply.github.com>
2023-08-06 11:37:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
